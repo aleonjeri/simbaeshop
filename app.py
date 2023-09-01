9c4d52ca1f88d7a5c0486f09618d6cc56af406c9
Shanaces <shanaces56@gmail.com>
2023-09-01 12:28:03 +0300

    projectaleo

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ def vendor_login():
         else:
             # session: Store Information About a specific user
             user_record = cursor.fetchone()
+            
             session['key'] = user_record[1]
             session['vendor_id'] = user_record[0]
             session['contact'] = user_record[2]
@@ -115,42 +116,7 @@ def add_product():
 
     else:
         return render_template('vendor_profile.html', message='Please Add Product Details')
-
-
-app.run(debug=True)
-
-@app.route('/add_product', methods=['POST', 'GET'])
-def add_product():
-    if request.method == 'POST':
-        product_name = request.form['name']
-        product_desc = request.form['desc']
-        product_cost = request.form['cost']
-        product_discount = request.form['discount']
-        product_category = request.form['category']
-        product_brand = request.form['brand']
-        product_image = request.files['image']
-        product_image.save('static/products/' + product_image.filename)
-
-        vendor_id = request.form['vendor']
-
-        connection = pymysql.connect(
-            host='localhost', user='root', password='', database='simbadb')
-
-        cursor = connection.cursor()
-
-        data = (product_name, product_desc, product_cost, product_discount,
-                product_category, product_brand, product_image.filename, vendor_id)
-
-        sql = "insert into products (product_name, product_desc, product_cost, product_discount, product_category, product_brand, product_image, vendor_id) values (%s, %s, %s, %s, %s, %s, %s, %s)"
-
-        cursor.execute(sql, data)
-        connection.commit()
-        return render_template('vendor_profile.html', message='Product Added Successfully')
-
-    else:
-        return render_template('vendor_profile.html', message='Please Add Product Details')
-
-
+    
 @app.route('/view_products')
 def view_products():
     connection = pymysql.connect(
@@ -170,5 +136,57 @@ def view_products():
     else:
         data = cursor.fetchall()
         return render_template('view_products.html', products=data)
+
+
+@app.route('/buy_products')
+def buy_products():
+    connection = pymysql.connect(
+        host='localhost', user='root', password='', database='simbadb')
+
+    # phones
+    cursor_phones = connection.cursor()
+    sql_phones = "select * from products where product_category = 'phones' "
+    cursor_phones.execute(sql_phones)
+    phones = cursor_phones.fetchall()
+
+    # laptops
+    cursor_laptops = connection.cursor()
+    sql_laptops = "select * from products where product_category = 'laptops'"
+    cursor_laptops.execute(sql_laptops)
+    laptops = cursor_laptops.fetchall()
+
+    # shoes
+    cursor_shoes = connection.cursor()
+    sql_shoes = "select * from products where product_category = 'shoes'"
+    cursor_shoes.execute(sql_shoes)
+    shoes = cursor_shoes.fetchall()
+
+    return render_template('buy_products.html', phones=phones, laptops=laptops, shoes=shoes)
+
+@app.route('/single_item/<product_id>')
+def single_item(product_id):
+    connection = pymysql.connect(
+        host='localhost', user='root', password='', database='simbadb')
+
+    cursor = connection.cursor()
+
+    sql = "select * from products where product_id = %s"
+
+    cursor.execute(sql, product_id)
+
+    single_record = cursor.fetchone()
+
+
+    
+
+    category = single_record[5]
+
+    cursor_similar = connection.cursor()
+    sql_similar = "select * from products where product_category = %s ORDER BY RAND() limit 3"
+    cursor_similar.execute(sql_similar, category)
+
+    similar_products = cursor_similar.fetchall()
+
+    return render_template('single_item.html', single_record=single_record, similar_products=similar_products)
 
 app.run(debug=True)
